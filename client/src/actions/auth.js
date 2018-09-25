673990010c4f8efdcf71b7e063c844f360823ee4
Meaghan Crowell <meaghan.crowell@gmail.com>
2018-09-25 13:49:52 -0400

    login working

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,90 +1,121 @@
-import { push } from 'connected-react-router';
+import { push } from "connected-react-router";
 
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAIL = 'REGISTER_FAIL';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const IS_LOGGING_IN = "IS_LOGGING_IN";
+export const GET_ONE_USER = "GET_USER";
 
-export const registerUser = ({ firstName , lastName , email , password }) => ( dispatch ) => {
+export const registerUser = ({
+  firstName,
+  lastName,
+  email,
+  password
+}) => dispatch => {
   const header = {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      "firstName": firstName,
-      "lastName": lastName,
-      "email": email,
-      "password": password
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
     })
-  }
+  };
 
-  fetch('/api/auth/register' , header )
-    .then( response => response.json() )
-    .then( registerResponse => {
-      console.log(registerResponse)
-      if ( registerResponse.hasOwnProperty( 'errors' )){
-        dispatch( registerFail() )
+  fetch("/api/auth/register", header)
+    .then(response => response.json())
+    .then(registerResponse => {
+      console.log(registerResponse);
+      if (registerResponse.hasOwnProperty("errors")) {
+        dispatch(registerFail());
       } else {
-        dispatch( registerSuccess( registerResponse.name , registerResponse.userId , registerResponse.token ));
-        dispatch( push ( '/' ))
+        dispatch(
+          registerSuccess(
+            registerResponse.name,
+            registerResponse.userId,
+            registerResponse.token
+          )
+        );
+        dispatch(push("/"));
       }
-    })
-}
+    });
+};
 
-export const registerSuccess = ( name , userId ) => {
+export const registerSuccess = (name, userId) => {
   return {
     type: REGISTER_SUCCESS,
     payload: {
       name,
       userId
     }
-  }
-}
+  };
+};
 
 export const registerFail = () => {
   return {
-    type: REGISTER_FAIL,
-  }
-}
+    type: REGISTER_FAIL
+  };
+};
 
-export const loginUser = ({ email , password }) => ( dispatch ) => {
+export const loginUser = ({ email, password }) => dispatch => {
+  dispatch(isLoggingIn());
+
   const header = {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      "email": email,
-      "password": password
+      email: email,
+      password: password
     })
-  }
+  };
 
-  fetch('/api/auth/login' , header )
-    .then( response => response.json() )
-    .then( loginResponse => {
-      console.log(loginResponse)
-      if ( loginResponse.hasOwnProperty( 'errors' )){
-        dispatch( loginFail() )
+  fetch("/api/auth/login", header)
+    .then(response => response.json())
+    .then(loginResponse => {
+      console.log(loginResponse);
+      if (!loginResponse) {
+        dispatch(loginFail());
+      } else if (loginResponse.hasOwnProperty("errors")) {
+        dispatch(loginFail());
       } else {
-        dispatch( loginSuccess( loginResponse.userId , loginResponse.token ));
-        dispatch( push ( '/' ))
+        dispatch(loginSuccess(loginResponse));
+        dispatch(push("/"));
       }
-    })
-}
+    });
+};
 
-export const loginSuccess = ( userId, token ) => {
+export const loginSuccess = user => {
   return {
     type: LOGIN_SUCCESS,
     payload: {
-      userId, 
-      token
+      user,
     }
-  }
-}
+  };
+};
 
 export const loginFail = () => {
   return {
-    type: LOGIN_FAIL,
-  }
-}
+    type: LOGIN_FAIL
+  };
+};
+
+export const isLoggingIn = () => {
+  return {
+    type: IS_LOGGING_IN
+  };
+};
+
+export const getOneUser = () => dispatch => {
+  fetch("/api/user")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.user);
+      dispatch({ type: GET_ONE_USER, convos: data.user });
+    });
+};
--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,12 @@
- 
+const express = require('express')
+const { User } = require('../models')
+const user = express.Router()
+
+
+
+
+
+
+module.exports = {
+  user
+}
