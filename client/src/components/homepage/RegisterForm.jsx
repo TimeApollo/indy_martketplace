44b342136bc6b932a858ae80c186628d8792ba26
TimeApollo <arnjcksn0845@yahoo.com>
2018-09-26 17:31:10 -0400

    working on register form

--- client/src/components/homepage/RegisterForm.jsx
+++ client/src/components/homepage/RegisterForm.jsx
@@ -1,46 +1,118 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { registerUser } from '../../actions/auth'
+import React from "react";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/auth";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+    display: "flex",
+    justifyContent: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "-1.5em",
+    width: "7em"
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit
+  },
+  header: {
+    marginBottom: "0px",
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    marginTop: "3em"
+  },
+  form: {
+    margin: "3em",
+    marginTop: "0",
+    border: "4px solid black",
+    width: "30em",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    marginLeft: "auto",
+    marginRight: "auto"
+  }
+});
 
 class RegisterForm extends React.Component {
-
   state = {
-    firstName: '',
-    lastName: '',
-    password: '',
-    passwordMatch: '',
-    email: '',
-  }
+    firstName: "",
+    lastName: "",
+    password: "",
+    passwordMatch: "",
+    email: ""
+  };
 
-  handleChange = ( event ) => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   handleRegisterUser = () => {
     let regFormData = {
       firstName: this.state.firstName,
       lastName: this.state.lastName,
       password: this.state.password,
-      email: this.state.email,
-    }
+      email: this.state.email
+    };
 
     //testing purposes. delete
     regFormData = {
-      firstName: 'Aaron',
-      lastName: 'Jackson',
-      password: '12345678',
-      email: 'aj@gm',
-    }
+      firstName: "Aaron",
+      lastName: "Jackson",
+      password: "12345678",
+      email: "aj@gm"
+    };
 
-    this.props.registerUser( regFormData )
-  }
+    this.props.registerUser(regFormData);
+  };
 
-  render(){
-    return(
+  render() {
+    const { classes } = this.props;
+
+    return (
       <div>
-        <button onClick={ this.handleRegisterUser }></button>
+        <h1 className={classes.header}>
+          LOGIN
+        </h1>
+        <div className={classes.form}>
+          <FormControl style={{ margin: "1em" }} required='true'>
+            <InputLabel>First Name</InputLabel>
+            <Input 
+              type="text" 
+              name="firstName"
+              value={this.state.firstName} 
+              onChange={this.handleChange} 
+              required='true'
+            />
+          </FormControl>
+          <FormControl style={{ margin: "1em" }}>
+            <InputLabel>Email</InputLabel>
+            <Input type="email" name="email" onChange={this.handleOnChange} />
+          </FormControl>
+          <FormControl style={{ margin: "1em" }}>
+            <InputLabel>Password</InputLabel>
+            <Input
+              type="password"
+              name="password"
+              onChange={this.handleOnChange}
+            />
+          </FormControl>
+        </div>
+        <Button
+          variant="extendedFab"
+          className={classes.button}
+          onClick={this.handleRegisterUser}
+        >
+          Register
+        </Button>
       </div>
-    )
+    );
   }
 }
 
@@ -48,12 +120,19 @@ const mapStateToProps = ({ auth }) => ({
   register: auth.register
 });
 
-const mapDispatchToProps = ( dispatch ) => {
+const mapDispatchToProps = dispatch => {
   return {
-    registerUser: ( regFormData ) => {
-      dispatch(registerUser( regFormData ))
+    registerUser: regFormData => {
+      dispatch(registerUser(regFormData));
     }
-  }
-}
+  };
+};
 
-export default connect( mapStateToProps , mapDispatchToProps )( RegisterForm )
+RegisterForm.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(RegisterForm));
